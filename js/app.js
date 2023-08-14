038a895f17888e12529ce1f5739497119b4fe08c
full-stack-david <davidchan12323@gmail.com>
2023-08-14 04:00:10 -0500

    Add Contact page

--- js/app.js
+++ js/app.js
@@ -6,6 +6,24 @@
     $(".menu-toggle").click(function(){
 			$(".mobile-navigation").slideToggle();
 		});
+
+    let map = $(".map");
+		let latitude = map.data("latitude");
+		let longitude = map.data("longitude");
+		if (map.length){
+			map.gmap3({
+				map:{
+					options:{
+						center: [latitude,longitude],
+						zoom: 15,
+						scrollwheel: false
+					}
+				},
+				marker:{
+					latLng: [latitude,longitude],
+				}
+			});	
+		}
   });
 
   $(window).load(function(){
