8f1036506a0250392d4f994d78a8e3736e4d7917
full-stack-david <davidchan12323@gmail.com>
2023-08-14 01:57:32 -0500

    Add navigation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 (function($, document, window){
 	
 	$(document).ready(function(){
+    $(".mobile-navigation").append($(".main-navigation .menu").clone());
 
+    $(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
   });
 
   $(window).load(function(){
