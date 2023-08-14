a71afc6713bd348d5b5c9831bf68e80705e017f8
full-stack-david <davidchan12323@gmail.com>
2023-08-14 02:32:33 -0500

    Add content & complete homepage

--- js/plugins.js
+++ js/plugins.js
@@ -4,4 +4,10 @@
 		var image = $(this).data("bg-image");
 		$(this).css("background-image", "url("+image+")");
 	});
+
+  var elBgClr = "[data-bg-color]";
+	$(elBgClr).each(function(){
+		var color = $(this).data("bg-color");
+		$(this).css("background-color", color );
+	});
 }(jQuery);
